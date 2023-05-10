c7fe5df6900f43fce1047abf5c6fc19e8d23c02d
Ling Fung Chan <justinlfchan@gmail.com>
2023-05-11 00:25:07 +0100

    added move-tile function to move the tile to the empty grid

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const tiles = document.querySelectorAll("td");
 // Check if a tile has an empty neighbor
 
 // Move the tile
+const moveTile = (el) => {
+  const emptyTile = document.querySelector(".empty");
+  console.log(emptyTile.innerHTML);
+  console.log(el.innerHTML);
+
+  emptyTile.innerHTML = el.innerHTML;
+  el.innerHTML = "";
+  empty.classList.remove("empty");
+  el.classList.add("empty");
+};
 
 // Add event listener on each tile
 tiles.forEach((tile) => {
